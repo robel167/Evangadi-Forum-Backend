963e0f87b40578ad94ff613a26f60a56d8d1f319
robel167 <robeldemsew1@gmail.com>
2024-05-31 14:49:15 +0300

    evangadi-forum backend commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 3307;
+const port = process.env.PORT;
 
 //json middleware to extract json data
 app.use(express.json());
