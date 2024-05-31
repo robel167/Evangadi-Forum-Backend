fa533727c71722d67274a571f013e0b46715d6d9
robel167 <robeldemsew1@gmail.com>
2024-05-31 09:29:42 +0300

    evangadi-forum backend

---
+++ db/dbConfig.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+let mysql2 = require("mysql2");
+const dbConnection = mysql2.createPool({
+    user: process.env.USER,
+    database: process.env.DATABASE,
+    host: "localhost",
+    port: 3307,
+    password: process.env.PASSWORD,
+    connctionLimit: 10,
+});
+
+console.log(process.env.DATABASE);
+module.exports = dbConnection.promise();
